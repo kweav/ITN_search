30ab723daf9d5618b8bbc045b2eb8551981f2fcf
Kate Isaac <41767733+kweav@users.noreply.github.com>
2024-08-07 15:02:57 -0400

    try to adjust to match main

--- scripts/format-tables.R
+++ scripts/format-tables.R
@@ -6,18 +6,18 @@ library(DT)
 library(here)
 
 #' Format the tibble/table to bold names, link links, add icons, and rearrange/select columns
-#' 
+#'
 #' @description This function uses the CourseName, Funding, CurrentOrFuture, GithubLink, BookdownLink, CourseraLink, and LeanpubLink columns in order to make the first two columns
 #' of the outputdf. For the first column of outputdf: CourseName input column value is bolded and icons are added below the name to denote the funding source(s) based on the values in the Funding input column.
 #' For the second column of outputdf: If the course is a current course, GithubLink, BookdownLink, CourseraLink, and LeanpubLink are linked to corresponding icons. If either CourseraLink or LeanpubLink are NA values, then those icons/links are not added
 #' If the course is a future course, the second column of outputdf instead has an under construction icon.
-#' For the third and fourth columns of outputdf: we select the WebsiteDescription and BroadAudience columns. 
-#' 
+#' For the third and fourth columns of outputdf: we select the WebsiteDescription and BroadAudience columns.
+#'
 #' @param inputdf the input data frame or tibble
 #' @param current a boolean; if TRUE (default), works with current courses; if FALSE, works with future courses by filtering on values in the CurrentOrFuture column
 #' @param keep_category a boolean; if FALSE (default), it won't keep the Category or Concepts columns. If TRUE, the Category column is kept after Links but before WebsiteDescription and the Concepts column is kept at the end
-#' @return outputdf the formatted table 
- 
+#' @return outputdf the formatted table
+
 
 prep_table <- function(inputdf, current=TRUE, keep_category = FALSE){
   if (current){
@@ -45,8 +45,8 @@ prep_table <- function(inputdf, current=TRUE, keep_category = FALSE){
       filter(CurrentOrFuture == "Future") %>% #select just future classes
       mutate(Links = '<img src="resources/images/underconstruction.png" height="40"></img>')
   }
-  
-  outputdf %<>% 
+
+  outputdf %<>%
     mutate(CourseName = paste0("<b>", CourseName, "</b>")) %>% #mutate the name to be bolded
     mutate(CourseName = #add logos for funding and link to appropriate about pages
              case_when(
@@ -59,12 +59,12 @@ prep_table <- function(inputdf, current=TRUE, keep_category = FALSE){
     mutate(Concepts = str_replace_all(Concepts, ";", "<br></br>- ")) %>%
     mutate(BroadAudience = str_replace_all(BroadAudience, ";", "<br></br>")) %>%
     mutate(BroadAudience = str_replace(BroadAudience, "Software developers", "<img src=\"resources/images/keyboard-1405.png\" alt=\"Software developers\" height=\"40\"></img><p class=\"image-name\">Software developers</p>")) %>%
-    mutate(BroadAudience = str_replace(BroadAudience, "New to data science", "<img src=\"resources/images/hatching_chick_newlearner.png\" alt =\"New to data science\" height=\"40\"></img><p class=\"image-name\">New to data science</p>")) %>%
+    mutate(BroadAudience = str_replace(BroadAudience, "Researchers", "<img src=\"resources/images/hatching_chick_newlearner.png\" alt =\"Researchers\" height=\"40\"></img><p class=\"image-name\">Researchers</p>")) %>%
     mutate(BroadAudience = str_replace(BroadAudience, "Leaders", "<img src=\"resources/images/leaders.png\" alt=\"Leaders\" height=\"40\"></img><p class=\"image-name\">Leaders</p>"))
-  
+
   #Replace the broad audiences with logos
-  
-  if (keep_category){ #select appropriate columns 
+
+  if (keep_category){ #select appropriate columns
     outputdf %<>% select(c(CourseName, Links, WebsiteDescription, BroadAudience, Category)) %>%
       `colnames<-`(c("Course Name", "Access the Course", "Description", "Broad Audience", "Category"))
   } else{
@@ -76,16 +76,16 @@ prep_table <- function(inputdf, current=TRUE, keep_category = FALSE){
 
 
 #' A function to setup the DT datatable
-#' 
+#'
 #' @description
 #' A short description...
-#' 
-#' 
+#'
+#'
 #' @param inputdf input dataframe or tibble to be displayed with the DT library
 #' @param some_caption a caption describing the table
-#' 
+#'
 #' @return output_table the DT datatable ready to display version of the inputdf
-#' 
+#'
 
 setup_table <- function(inputdf, some_caption, columnDefsListOfLists=NULL){
   if (is.null(columnDefsListOfLists)){
@@ -119,73 +119,73 @@ setup_table <- function(inputdf, some_caption, columnDefsListOfLists=NULL){
 #'
 
 write_Rmd <- function(courseROI){
-  
+
   # Rmd Header information
   cat(paste0('---\ntitle: "', courseROI$CourseName, '"\noutput: html_document\n---\n\n'))
-  
+
   # Rmd styling information
   cat('<style type = "text/css">\n\tbody{\n\ttext-align: justify;\n\ttext-justify: inter-word;\n}\n</style>\n\n<style>\n\tpre{\n\t\tborder: 0;\n\t}\n</style>\n```{r include=FALSE}\nknitr::opts_chunk$set(comment=NA)\n```\n\n')
-  
+
   # import ottrpal
-  
+
   cat('```{r, echo=FALSE, results=\'hide\',message=FALSE,warning=FALSE}\n\nlibrary(ottrpal)\n\n```')
-  
-  # About the course info  
-  
+
+  # About the course info
+
   cat(paste0('## About this course\n\n```{r echo=FALSE,message=FALSE, warning=FALSE,results = \'asis\'}\ncat(courseROI$WebsiteDescription)\n```\n\nThis course is one of our **', courseROI$Category, '** Courses.\n\n'))
-  
+
   # For individuals who info
-  
+
   cat(paste0('## For individuals who\n\n```{r echo=FALSE,message=FALSE,warning=FALSE,out.width="100%"}\nottrpal::include_slide(courseROI$ForIndividualsWhoSlideLink\n```\n\nWe particularly recommend this course for those who are **', str_replace(courseROI$BroadAudience, "; ", "** or **"), '**\n\n'))
-  
-  if (!is.na(str_match(courseROI$BroadAudience, "New to data science"))){
-    cat('![New to data science](resources/images/NewToDataScience.png){width=10% height=10%}\n\n')
+
+  if (!is.na(str_match(courseROI$BroadAudience, "Researchers")) | !is.na(str_match(courseROI$BroadAudience, "New to data science"))){
+    cat('![Researchers](resources/images/NewToDataScience.png){width=10% height=10%}\n\n')
   }
-  
+
   if (!is.na(str_match(courseROI$BroadAudience, "New to data science"))){
     cat('![Software Developer](resources/images/SoftwareDeveloper.png){width=10% height=10%}\n\n')
   }
-  
+
   if (!is.na(str_match(courseROI$BroadAudience, "Leaders"))){
     cat('![Leader](resources/images/leaders.png){width=10% height=10%}\n\n')
   }
-  
+
   # For Concepts Discussed Info
-  
+
   cat('## Concepts Discussed\n\n```{r echo=FALSE,message=FALSE,warning=FALSE, out.width = "100%"}\nottrpal::include_slide(courseROI$ConceptsDiscussedSlideLink)\n```\n\n')
-  
+
   # For Learning Objectives Info
-  
+
   cat('## Learning Objectives\n\n```{r echo=FALSE,message=FALSE,warning=FALSE,out.width="100%"}\nottrpal::include_slide(courseROI$LOSlideLink)\n```\n\n')
-  
+
   # Prerequisites info if applicable
-  
+
   if (!is.na(courseROI$PrereqsLink)){
     cat('## Prerequisites\n\n```{r echo=FALSE,message=FALSE,warning=FALSE,out.width="100%"}\nottrpal::include_slide(courseROI$PrereqsLink\n```\n\n')
   }
-  
+
   # Course formats info
-  
+
   cat('## Available course formats to access this course\n\nThis course is available in the following formats:\n\n- [on our bookdown website](`r courseROI$BookdownLink`) (easy to access, free, & self-guided/no quizzes)\n')
-  
+
   if (!is.na(courseROI$CourseraLink)){
     cat('- [on Coursera](`r courseROI$CourseraLink`) (completion comes with certification; not free unless institutional login available)\n')
   }
-  
+
   if (!is.na(courseROI$LeanpubLink)){
     cat('- [on Leanpub](`r courseROI$LeanpubLink`) (completion comes with certification; free access)\n')
   }
-  
+
   cat('- [GitHub source material](`r courseROI$GithubLink`)\n\n')
-  
+
   # Funding info
-  
+
   cat('## Funding\n\n[![ITCR](resources/images/ITCRLogo.png){width=10% height=10%}](https://itcr.cancer.gov/)')
-  
+
   if (!is.na(str_match(courseROI$Funding, "Hutch"))){
     cat('[!Fred Hutch Cancer Center](resources/images/fhlogo.png){width=10% height=10%}](https://www.fredhutch.org/en/about/about-the-hutch.html)')
   }
-  
+
   # Need to make a function that wraps this function in a writeLines(), giving it each course row to create a new Rmd (that will be rendered by OTTR)
-  
+
 }
